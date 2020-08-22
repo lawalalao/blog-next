b138a15b4ecf1992f1cba2517f57f048609b66da
Catalin Pit <catalinpit@gmail.com>
2020-08-22 17:44:18 +0100

    Added SM icons

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,21 @@ export default function Home({ postsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p className={utilStyles.introductoryParagraph}><em>"It is your attitude, more than your aptitude, that will determine your altitude"</em></p>
+      <p className={utilStyles.introductoryParagraph, utilStyles.inline}>
+          <a href='https://twitter.com/catalinmpit'>
+            <img src='https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/twitter.svg' alt='twitter' height='20' />
+          </a>
+          <a href='https://www.instagram.com/catalinmpit'>
+            <img src='https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/instagram.svg' alt='instagram' height='20' />
+          </a>
+          <a href='https://www.linkedin.com/in/catalinpit'>
+            <img src='https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/linkedin.svg' alt='linkedin' height='20' />
+          </a>
+          <a href='https://dev.to/catalinmpit'>
+            <img src='https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/dev-dot-to.svg' alt='twitter' height='20' />
+          </a>
+        </p>
+        <p className={utilStyles.introductoryParagraph}><em>'It is your attitude, more than your aptitude, that will determine your altitude'</em></p>
         <p className={utilStyles.introductoryParagraph}>Software Engineer ∙ Content creator ∙ Tools agnostic ∙ Technical writer</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
@@ -20,7 +34,7 @@ export default function Home({ postsData }) {
         <ul className={utilStyles.list}>
           {postsData.map(({ postID, title, date, snippet }) => (
             <li className={utilStyles.listItem} key={postID}>
-              <Link href="/[postID]" as={`/${postID}`}>
+              <Link href='/[postID]' as={`/${postID}`}>
                 <a>{title}</a>
               </Link>
               <br />
